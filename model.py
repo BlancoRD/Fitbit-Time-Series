550669c053c346e9012803b42678d6c539a217ab
Cris Giovanoni <giovanonicris@gmail.com>
2019-11-21 00:37:07 -0600

    setup and model

--- model.py
+++ model.py
@@ -1,128 +1,89 @@
-#target_vars = train.columns.to_list()
-#eval_df = pd.DataFrame(columns=['model_type', 'target_var', 'metric', 'value'])
+# data manipulation 
+import numpy as np
 import pandas as pd
+
+from statsmodels.tsa.api import ExponentialSmoothing, SimpleExpSmoothing, Holt
+import statsmodels.api as sm
 from sklearn import metrics
 import math
 
-def evaluate(target_var, train, test, output=True):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    
-    mse = metrics.mean_squared_error(test[target_var], yhat[target_var])
-    rmse = math.sqrt(mse)
+# data visualization 
+import matplotlib
+import matplotlib.pyplot as plt
+import seaborn as sns
 
-    if output:
-        print('MSE:  {}'.format(mse))
-        print('RMSE: {}'.format(rmse))
-    else:
-        return mse, rmse
-    
-def append_eval_df(model_type, target_vars, train , test, eval_df):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    
-    temp_eval_df = pd.concat([pd.DataFrame([[model_type, i, 'mse', evaluate(target_var = i, 
-                                                                             train = train, 
-                                                                             test = test, 
-                                                                            output=False)[0]],
-                                            [model_type, i, 'rmse', evaluate(target_var = i, 
-                                                                              train =train, 
-                                                                              test =test, 
-                                                                             output=False)[1]]],
-                                           columns=['model_type', 'target_var', 'metric', 'value']) 
-                              for i in target_vars], ignore_index=True)
-    eval_df = eval_df.append(temp_eval_df, ignore_index=True)
-    return eval_df
 
-def plot_and_eval(target_vars, train , test ,metric_fmt = '{:.2f}', linewidth = 4):
-    import pandas as pd
-    from sklearn import metrics
-    import math    
-    if type(target_vars) is not list:
-        target_vars = [target_vars]
+#MODELS
 
-    plt.figure(figsize=(16, 8))
-    plt.plot(train[target_vars],label='Train', linewidth=1)
-    #plt.plot(test[target_vars], label='Test', linewidth=1)
+def forecast_simple(attribute,train):
+    simple = train[attribute].mean()
+    return simple
 
-    for var in target_vars:
-        mse, rmse = evaluate(target_var = var,  train = train,  test =test, output=False)
-        plt.plot(yhat[var], linewidth=linewidth)
-        print(f'{var} -- MSE: {metric_fmt} RMSE: {metric_fmt}'.format(mse, rmse))
-    
-    plt.show()
+def forecast_moving(attribute,train):
+    moving = train[attribute].rolling(7).mean().iloc[-1]
+    return moving
 
-def last_observed(train, test, target_vars, eval_df):
-    import pandas as pd
-    from sklearn import metrics
-    import math    
-    yhat = pd.DataFrame(test[target_vars])
-    for var in target_vars:
-        yhat[var] = int(train[var][-1:])
-        eval_df = append_eval_df('last_observed', target_vars, train, test, eval_df)
-    return eval_df
+def forecast_holt_linear(attribute,train,test,smoothing_level=.5,smoothing_slope=.5):
+    holt = Holt(train[attribute]).fit(smoothing_level, smoothing_slope, optimized=False)
+    holt_yhat = holt.forecast(test[attribute].shape[0])
+    holt_yhat = pd.DataFrame(holt_yhat,index=test.index)
+    return holt_yhat
 
-def simple_avg(train, test, target_vars):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    yhat = pd.DataFrame(test[target_vars])
-    for var in target_vars:
-        yhat[var] = train[var].mean()
-    eval_df = append_eval_df('simple_avg', target_vars, train, test)
-    return eval_df
+def forecast_holt_es(attribute,train,test,smoothing_level=.5,smoothing_slope=.9,smoothing_seasonal=0.4):
+    holtes = ExponentialSmoothing(train[attribute], trend="add", seasonal_periods=7).fit(smoothing_level, smoothing_slope, smoothing_seasonal)
+    holtes_yhat = holtes.forecast(test[attribute].shape[0])
+    holtes_yhat = pd.DataFrame(holtes_yhat,index=test.index)
+    return holtes_yhat
 
-def moving_avg(train, test, target_vars):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    periods = 30
-    periods = 30
-    for var in target_vars:
-        yhat[var] = train[var].rolling(periods).mean().iloc[-1]
 
-    plot_and_eval(target_vars, train, test)
-    eval_df1 = append_eval_df(model_type='moving_average', target_vars=target_vars, train = train, test = test)
+#CONNECTORS
 
-    plt.figure(figsize=(16, 8))
-    plt.plot(train[target_vars],label='Train')
-    plt.plot(test[target_vars], label='Test')
-    period_vals = [1, 4, 12, 26, 52, 104]
-    for p in period_vals:
-        for var in target_vars:
-            yhat[var] = train[var].rolling(p).mean().iloc[-1]
-            plt.plot(yhat[var])
-            print('\nrolling averge period:',p)
-            print(var)
-            evaluate(var, train,  test)
-    eval_df2 = append_eval_df('moving_avg', target_vars,  train,  test)
-    return eval_df1, eval_df2
+def tabulate_forecasts(attribute,test,train,hsl=.5,hss=.5,hesl=.5,hess=.9,hesnl=.4):
+    tab = pd.DataFrame(data=test[attribute], index=test.index)
+    tab["simple"] = forecast_simple(attribute,train)
+    tab["moving"] = forecast_moving(attribute,train)
+    tab["holt_yhat"] = forecast_holt_linear(attribute,train,test,hsl,hss)
+    tab["holtes_yhat"] = forecast_holt_es(attribute,train,test,hesl,hess,hesnl)
+    return tab
 
-def holt_linear(train, test, target_vars):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    from statsmodels.tsa.api import Holt
+def get_rmse_list(attribute,test,train,hsl=.5,hss=.5,hesl=.5,hess=.9,hesnl=.4):
+    tab = tabulate_forecasts(attribute,test,train,hsl,hss,hesl,hess,hesnl)
+    rmses = []
+    simple = rmses.append(math.sqrt(metrics.mean_squared_error(tab[attribute], tab.simple)))
+    moving = rmses.append(math.sqrt(metrics.mean_squared_error(tab[attribute], tab.moving)))
+    holt = rmses.append(math.sqrt(metrics.mean_squared_error(tab[attribute], tab.holt_yhat)))
+    holtes = rmses.append(math.sqrt(metrics.mean_squared_error(tab[attribute], tab.holtes_yhat)))
+    return rmses
 
-    for var in target_vars:
-        model = Holt(train[var]).fit(smoothing_level=.3, smoothing_slope=.1, optimized=False)
-        yhat[var] = pd.DataFrame(model.forecast(test[var].shape[0]), columns=[var])
+def get_all_rmse(test,train):
+    rmsey = get_rmse_list("cal_activity",test,train)
+    rmse_steps =  get_rmse_list("steps",test,train,hsl=.1,hss=.2,hesl=.1,hess=.13)
+    rmse_distance =  get_rmse_list("distance",test,train,hsl=.32,hss=.1,hesl=.01,hess=.01)
+    rmse_floors =  get_rmse_list("floors",test,train,hsl=.32,hss=.1,hesl=.01,hess=.01)
+    rmse_light =  get_rmse_list("min_active_light",test,train,hsl=.32,hss=.1,hesl=.05,hess=.01)
+    rmse_fairly =  get_rmse_list("min_active_fairly",test,train,hsl=.08,hss=.12,hesl=.05,hess=.01)
+    rmse_very =  get_rmse_list("min_active_very",test,train,hsl=.12,hss=.2,hesl=.01,hess=.01)
+    return rmsey, rmse_steps, rmse_distance, rmse_floors, rmse_light, rmse_fairly, rmse_very
 
-    #plot_and_eval(target_vars, train, test)
-        eval_df = append_eval_df(model_type='holts_linear_trend', target_vars=target_vars,  train =train,  test =test)
-    return eval_df
+def generate_rmse_table(rmsey, rmse_steps, rmse_distance, rmse_floors, rmse_light, rmse_fairly, rmse_very):
+    method_label = ["simple","moving","holt_linear","holt_expo"]
 
-def holt_exp(train, test, target_vars):
-    import pandas as pd
-    from sklearn import metrics
-    import math
-    for var in target_vars:
-        from statsmodels.tsa.holtwinters import ExponentialSmoothing
-        model = ExponentialSmoothing(train[var], trend= 'additive').fit(smoothing_level=.3, smoothing_slope=.1, optimized=False)
-        yhat[var] = pd.DataFrame(model.forecast(test[var].shape[0]), columns=[var])
+    cal_act = pd.DataFrame(index=method_label,data=rmsey,columns=["cal_act"]).T
+    steps = pd.DataFrame(index=method_label,data=rmse_steps,columns=["steps"]).T
+    distance = pd.DataFrame(index=method_label,data=rmse_distance,columns=["distance"]).T
+    floors = pd.DataFrame(index=method_label,data=rmse_floors,columns=["floors"]).T
+    light = pd.DataFrame(index=method_label,data=rmse_light,columns=["light"]).T
+    fairly = pd.DataFrame(index=method_label,data=rmse_fairly,columns=["fairly"]).T
+    very = pd.DataFrame(index=method_label,data=rmse_very,columns=["very"]).T
 
-    #plot_and_eval(target_vars, train, test)
-        eval_df = append_eval_df(model_type='holts_exponential_trend', target_vars=target_vars,  train = train, test = test)
-    return eval_df
+    rmse_table = cal_act.append(steps)
+    rmse_table = rmse_table.append(distance)
+    rmse_table = rmse_table.append(floors)
+    rmse_table = rmse_table.append(light)
+    rmse_table = rmse_table.append(fairly)
+    rmse_table = rmse_table.append(very)
+
+    return rmse_table
+
+
+
